5b2e7de19902374c427ec70cbd54a43335d5c031
wangny <angelbubbleonly@gmail.com>
2016-05-12 20:33:27 +0800

    show on circle

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,11 +1,8 @@
 package main.java;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
-import processing.core.PFont;
 import processing.data.JSONArray;
 import processing.data.JSONObject;
 import controlP5.*;    // import controlP5 library
@@ -25,6 +22,9 @@ public class MainApplet extends PApplet{
 	Character chs;
 	private final static int width = 1200, height = 650;
 	
+	private int count;
+	
+	
 	public void setup() {
 		characters = new ArrayList<Character>();
 		size(width, height);
@@ -40,11 +40,19 @@ public class MainApplet extends PApplet{
 		.setPosition(width-200, height-600)
 		.setSize(100,50);
 		chs=new Character(this);
+		count = 0;
 	}
 
 	public void draw() {
 		
 		background(230);
+		noFill();
+		stroke(150, 200, 200);
+		strokeWeight(10);
+		ellipse (700,350, 500, 500);
+		
+		fill(150, 200, 200);
+		noStroke();
 		
 		int i=0;
 		int x = 60, y = 35;
@@ -58,6 +66,17 @@ public class MainApplet extends PApplet{
 				y += 60;
 			}
 		}
+		
+		i=0;
+		double rate = 0;
+		if(count>0) rate = 360/count;
+		for(Character ch : characters){
+			if(ch.isActivated()==true){
+				ch.display((int)(700+250*Math.sin(rate*i)), (int)(350-250*Math.cos(rate*i)) );
+			}
+			i++;
+		}
+		
 		for(Character ch : characters){
 			if(  Math.abs(this.mouseX - ch.x)<25 && Math.abs(this.mouseY - ch.y)<25 && ch.isActivated()==false){
 				fill(ch.colour);
@@ -67,31 +86,17 @@ public class MainApplet extends PApplet{
 				text(ch.name, ch.x, ch.y-10);
 			}
 		}
-		/*Graphics g = null;
-		g.setColor(Color.red);	*/
-		noFill();
-		stroke(150, 200, 200);
-		strokeWeight(10);
-		ellipse (700,350, 500,500);
-		
-		fill(150, 200, 200);
-		noStroke();
-		
-	/*	rect(1000, 40, 100, 50);
-		rect(1000, 150, 100, 50);
-		fill(255);
-		textSize(23);
-		this.text("Add All", 1005, 75);
-		this.text("Clear All", 1005, 185);*/
 	
 	}
 	
 	public void Clear(){
+		count = 0;
 		for(Character ch : characters)
 			ch.setActivate(false);
 		
 	}
 	public void Add(){
+		count = characters.size();
 		for(Character ch : characters)
 			ch.setActivate(true);
 	}
@@ -103,7 +108,6 @@ public class MainApplet extends PApplet{
 		int source;
 		int target;
 		int value;
-		int colour;
 		
 		 data=loadJSONObject(path+file);
 		 nodes=data.getJSONArray("nodes");
